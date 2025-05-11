ee2dbaa668e3113ade75196ffb9a68840c67608b
ANASETEL <148634360+ANASETEL@users.noreply.github.com>
2025-05-12 00:28:43 +0200

    Fix: corrected type for page params

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,9 +1,56 @@
-// /app/dashboard/invoices/[id]/edit/page.tsx
-import Form from '@/app/ui/invoices/edit-form';
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+// // /app/dashboard/invoices/[id]/edit/page.tsx
+// import Form from '@/app/ui/invoices/edit-form';
+// import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+// import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 
-export default async function Page({ params }: { params: { id: string } }) {
+// export default async function Page({ params }: { params: { id: string } }) {
+//   const id = params.id;
+
+//   const [invoice, customers] = await Promise.all([
+//     fetchInvoiceById(id),
+//     fetchCustomers(),
+//   ]);
+
+//   return (
+//     <main>
+//       <Breadcrumbs
+//         breadcrumbs={[
+//           { label: 'Invoices', href: '/dashboard/invoices' },
+//           {
+//             label: 'Edit Invoice',
+//             href: `/dashboard/invoices/${id}/edit`,
+//             active: true,
+//           },
+//         ]}
+//       />
+//       <Form invoice={invoice} customers={customers} />
+//     </main>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+import { InferGetServerSidePropsType } from 'next'; // No siempre necesario en App Router
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import Form from '@/app/ui/invoices/edit-form';
+
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function Page({ params }: PageProps) {
   const id = params.id;
 
   const [invoice, customers] = await Promise.all([
@@ -27,3 +74,4 @@ export default async function Page({ params }: { params: { id: string } }) {
     </main>
   );
 }
+
